49e179624b9d3399ceba9968e58b4fc93305e8c1
Sonam-Kumari-0104 <91051165+Sonam-Kumari-0104@users.noreply.github.com>
2022-02-21 22:37:21 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -1,10 +1,13 @@
 #include "typewise-alert.h"
 #include <stdio.h>
 
-int limits[NUMBEROFCOOLINGTYPES][NUMBEROFTEMPLIMITS] = {{0,35}, {0,45}, {0,40}};
-char alertMsgForEmail[NUMBEROFBREACHTYPES][100] = {"Hi, the temperature is Normal\n"
+int limits[NUMBER_OF_COOLING_TYPES][NUMBER_OF_TEMP_LIMITS] = {{PASSIVE_COOLING_LOWER_LIMIT, PASSIVE_COOLING_UPPER_LIMIT}, 
+                                                              {HI_ACTIVE_COOLING_LOWER_LIMIT, HI_ACTIVE_COOLING_UPPER_LIMIT}, 
+                                                              {MED_ACTIVE_COOLING_LOWER_LIMIT, MED_ACTIVE_COOLING_UPPER_LIMIT}};
+char alertMsgForEmail[NUMBER_OF_BREACH_TYPES][100] = {"Hi, the temperature is Normal\n"
                                                    "Hi, the temperature is too low\n",
                                                    "Hi, the temperature is too High\n"};
+void (*AlertTargetfp[NUMBER_OF_ALERT_TARGETS])(BreachType) = {sendToController, sendToEmail};
 
 BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
   if(value < lowerLimit) {
@@ -18,25 +21,16 @@ BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
 
 BreachType classifyTemperatureBreach(
     CoolingType coolingType, double temperatureInC) {
-  int lowerLimit = limits[coolingType][LOWERLIMITIDX];
-  int upperLimit = limits[coolingType][UPPERLIMITIDX];
+  int lowerLimit = limits[coolingType][LOWERLIMIT_IDX];
+  int upperLimit = limits[coolingType][UPPERLIMIT_IDX];
   return inferBreach(temperatureInC, lowerLimit, upperLimit);
 }
 
 void checkAndAlert(
     AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
 
-  BreachType breachType = classifyTemperatureBreach(
-    batteryChar.coolingType, temperatureInC
-  );
-
-  switch(alertTarget) {
-    case TO_CONTROLLER:
-      sendToController(breachType);
-      break;
-    case TO_EMAIL:
-      sendToEmail(breachType);
-      break;
+    BreachType breachType = classifyTemperatureBreach( batteryChar.coolingType, temperatureInC);
+    AlertTargetfp[alertTarget](breachType);
   }
 }
 
